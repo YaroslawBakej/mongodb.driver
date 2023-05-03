5741f6fee257e92bd51dacb83d80cbe577cb8e57
Yaroslaw Bakej <jarobak1995@gmail.com>
2023-05-03 23:06:16 +0300

    i'm the best

--- src/db.js
+++ src/db.js
@@ -1,11 +1,11 @@
 const { MongoClient } = require('mongodb')
 
 async function connect() {
-    const MongoDBclient = new MongoClient('mongodb://localhost:27017')
+    const MongoDBclient = new MongoClient('mongodb://127.0.0.1:27017')
 
     await MongoDBclient.connect()
 
-    const db = MongoDBclient.db('hschool');
+    const db = MongoDBclient.db('HSchool');
 
     return {
         students: db.collection('students')
--- src/repository/user.repository.js
+++ src/repository/user.repository.js
@@ -6,10 +6,15 @@ async function getAllUsersDB() {
     return await students.find().toArray();
 }
 async function getUserByIdDB(_id) {
+    console.log("+");
+    console.log(_id);
+    console.log(typeof _id);
     const { students } = await connect()
-    return await students.find({ _id: new ObjectId(_id) }).toArray();
+    return students.findOne({ _id: new ObjectId(_id) });
 }
 async function createUserDB(name, surname) {
+    console.log("+");
+    console.log(name, surname);
     const { students } = await connect()
     await students.insertOne({ name, surname });
     return 'success'
